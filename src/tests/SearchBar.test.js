8b76ad5a0bf438864fd3e30b44159a4e8759de78
enderst3 <enderst3@gmail.com>
2017-06-09 18:53:50 -0700

    more testing

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -6,6 +6,7 @@ import { shallow } from 'enzyme'
 
 describe('SearchBar', () => {
   let handleOnChange
+  let wrapper
 
   beforeEach(() => {
     handleOnChange = jest.fn()
@@ -14,12 +15,24 @@ describe('SearchBar', () => {
   it('will make the correct callback when adding text', () => {
     const searchBar = shallow(
       <SearchBar
-        handleOnChange={handleOnChange}
+        onSearchTermChange={handleOnChange}
       />
     )
     const textBox = searchBar.find('#drink-input')
     const event = {target: {value: 'gin'}}
     textBox.simulate('change', event)
     expect(handleOnChange).toBeCalledWith(event)
+  })
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <SearchBar />
+    )
+  })
+  it('It should have a FormControl element', () => {
+    expect(wrapper.find('#drink-input').exists()).toBe(true)
+  })
+  it('It should have a panel element', () => {
+    expect(wrapper.find('.SearchBar').exists()).toBe(true)
   })
 })
