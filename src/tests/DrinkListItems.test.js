7fdd0c0b34dcb15169f4ba9b2bb0ae07c2ad9ea6
enderst3 <enderst3@gmail.com>
2017-06-26 12:57:08 -0700

    fix travis issues

--- src/tests/DrinkListItems.test.js
+++ src/tests/DrinkListItems.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import DrinkListItem from '../DrinkListItem'
 import { shallow } from 'enzyme'
-<<<<<<< HEAD
 import {DRINK_DATA} from '../test-data'
 import fetch from 'jest-fetch-mock'
 global.fetch = fetch
@@ -37,23 +36,4 @@ describe('DrinkListItem', () => {
       done()
     }, 50)
   })
-=======
-
-/* global it describe expect beforeEach */
-
-describe('DrinkListItem', () => {
-  let wrapper
-
-  beforeEach(() => {
-    wrapper = shallow(
-      <DrinkListItem />
-    )
-  })
-  it('Should display image', () => {
-    expect(wrapper.find('.CocktailImage').exists()).toBe(true)
-  })
-  it('Should display drink name', () => {
-    expect(wrapper.find('.DrinkName').exists()).toBe(true)
-  })
->>>>>>> master
 })
